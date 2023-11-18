28b04976f384bba382ecaafc176bdafa5b245dca
Sang Tran <t@sang.jp>
2023-11-19 00:46:50 +0900

    build

--- index.mjs
+++ index.mjs
@@ -8,14 +8,18 @@ export default function makePublisher() {
 			// if callback is subscribed multiple times, it will be fired multiple times
 			// when removed, only last instance will be removed
 			subscribers.push(subscriber)
-			return () => {
+			return function unsubscribe() {
 				const idx = subscribers.lastIndexOf(subscriber)
-				if (idx >= 0) subscribers.splice(idx, 1)
+				if (idx >= 0)
+					subscribers.splice(idx, 1)
 			}
 		},
 		publish(payload) {
-			// if a subscriber has error, the following subscribers will not be called
-			for (const subscriber of subscribers) subscriber(payload)
+			for (const subscriber of subscribers)
+				try {
+					subscriber(payload)
+				} catch (_a) {
+				}
 		}
 	}
 }
